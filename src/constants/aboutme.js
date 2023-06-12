0132301b2274cdad148c30353e6eba6cafbb83dd
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-06-12 21:58:56 +0100

    About Me finished

---
+++ src/constants/aboutme.js
@@ -0,0 +1,90 @@
+const experiences = [
+  {
+    title: "Applications Consultant",
+    company_name: "Maintel",
+    type: "work",
+    date: "07/2018 - 02/2023",
+    description: [
+      "Completed pre-sales tasks, provided technical details for sales.",
+      "Wrote High Level Design for business and Low Level Design with technical details",
+      "Designed and developed a multilingual, database driven dynamic self-service project for a mobile provider with a lot of RESTful API integration.",
+      "Delivered a multilingual self-service project for a large European customer. Designed and developed non-factory text to speech grammars for Eastern European and North European languages.",
+      "Designed and developed a self-service application for a customer in the UK public sector using SMS and web chat APIs. ",
+    ],
+  },
+  {
+    title: "Technical Consultant",
+    company_name: "Cyara",
+    type: "work",
+    date: "11/2016 - 06/2018",
+    description: [
+      "Collaborated closely with the sales, pre-sales and marketing team.",
+      "Assisted customers to design test scenarios using Cyara products.",
+      "Collected test results, analysed data, and provided detailed feedback to the customer.",
+      "Delivered an automated, multichannel contact centre monitoring framework for a mobile provider.",
+      "Delivered training for a large bank’s testing department including managers and executives.",
+    ],
+  },
+  {
+    title: "Consultant",
+    company_name: "Geomant",
+    type: "work",
+    date: "08/2012 - 10/2016",
+    description: [
+      "Completed pre-sales tasks, provided technical details for sales.",
+      "Wrote High Level Design for business and Low Level Design with technical details",
+      "Lead Avaya self-service deployment and development projects with a small team.",
+      "Transitioned a City Council’s old contact centre to Avaya platform.",
+      "Designed and developed a complex multilingual self-service application using speech recognition and text to speech.",
+    ],
+  },
+  {
+    title: "Consultant",
+    company_name: "Geomant UK",
+    type: "work",
+    date: "08/2012 - 10/2016",
+    description: [
+      "Completed pre-sales tasks, provided technical details for sales.",
+      "Wrote High Level Design for business and Low Level Design with technical details",
+      "Lead Avaya self-service deployment and development projects with a small team.",
+      "Transitioned a City Council’s old contact centre to Avaya platform.",
+      "Designed and developed a complex multilingual self-service application using speech recognition and text to speech.",
+    ],
+  },
+  {
+    title: "Senior System Engineer",
+    company_name: "Geomant Hungary",
+    type: "work",
+    date: "04/2004 - 05/2012",
+    description: [
+      "Wrote Low Level Design with technical details.",
+      "Delivered Avaya training for colleagues and customers.",
+      "Designed and developed a full database driven self-service IVR project.",
+      "Developed text to speech grammars for Eastern European customers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Pixelpark Hungary",
+    type: "work",
+    date: "09/2000 - 12/2003",
+    description: [
+      "Developing web applications.",
+      "Travelled  to the UK and Austria to be part of an international developer team.",
+      "Developed a horizontal web portal for a large UK customer.",
+      "Did various web projects for Austrian customers.",
+    ],
+  },  
+  {
+    title: "Software Developer",
+    company_name: "Geomant Hungary",
+    type: "work",
+    date: "09/1998 - 08/2000",
+    description: [
+      "Developed Windows based multimedia applications ",
+      "Travelled to Germany to a customer site",            
+    ],
+  },  
+]
+
+export { experiences };
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,104 +1,48 @@
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import SchoolIcon from '@mui/icons-material/School';
 import WorkIcon from '@mui/icons-material/Work';
-import StarIcon from '@mui/icons-material/Star';
 
+import "styles/AboutMe.css";
+import { experiences } from "../constants/aboutme"; 
 
+const ElementCard = ({ experience }) => {
+  return (
+    <VerticalTimelineElement
+      className="vertical-timeline-element--work"
+      contentStyle={{ background: 'var(--tetriary-color)', color: '#fff' }}
+      contentArrowStyle={{ borderRight: '7px solid  var(--tetriary-color)' }}
+      date={ experience.date }
+      iconStyle={{ background: 'var(--primary-color)', color: '#fff' }}
+      icon={<WorkIcon />}
+    >
+      <h3 className="vertical-timeline-element-title">{ experience.title }</h3>
+      <h4 className="vertical-timeline-element-subtitle">{ experience.company_name }</h4>
+      <p>
+      <ul className='mt-5 list-disc ml-5 space-y-2'>
+          {experience.description.map((point, index) => (
+            <li
+              key={`experience-point-${index}`}
+              className='text-white-100 pl-1 tracking-wider'
+            >
+              {point}
+            </li>
+          ))}
+        </ul>
+      </p>
+    </VerticalTimelineElement> 
+  );
+}
 
 const AboutMe = () => {
   return ( 
-<VerticalTimeline>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    date="2011 - present"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    icon={<WorkIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Creative Director</h3>
-    <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-    <p>
-      Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    date="2010 - 2011"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    icon={<WorkIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Art Director</h3>
-    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-    <p>
-      Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    date="2008 - 2010"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    icon={<WorkIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Web Designer</h3>
-    <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-    <p>
-      User Experience, Visual Design
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    date="2006 - 2008"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    icon={<WorkIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Web Designer</h3>
-    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-    <p>
-      User Experience, Visual Design
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="April 2013"
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-    icon={<SchoolIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-    <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-    <p>
-      Strategy, Social Media
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="November 2012"
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-    icon={<SchoolIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-    <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-    <p>
-      Creative Direction, User Experience, Visual Design
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--education"
-    date="2002 - 2006"
-    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-    icon={<SchoolIcon />}
-  >
-    <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-    <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-    <p>
-      Creative Direction, Visual Design
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-    icon={<StarIcon />}
-  />
+<VerticalTimeline className="AboutMe">
+  {experiences.map((experience, index) => (
+    <ElementCard
+      key={`experience-${index}`}
+      experience={experience}
+    />
+  ))}
+   
 </VerticalTimeline>
    );
 }
